aed71c8977b304f324d865ea1346d219385a3401
Aiden Harrington <aidenkharrington@gmail.com>
2021-05-04 16:11:06 -0700

    WIP

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'The recipes',
+        ),
+      ),
+      body: Center(
+        child: Text(
+          'Recipes',
+        ),
+      ),
+    );
+  }
+}
