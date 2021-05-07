caaaa840e3ae0085acec8e1ebe2a8e51242300b5
Aiden Harrington <aidenkharrington@gmail.com>
2021-05-06 18:59:51 -0700

    WIP

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import './categories_screen.dart';
+import './favorite_screen.dart';
+
 class TabsScreen extends StatefulWidget {
   @override
   _TabsScreenState createState() => _TabsScreenState();
@@ -8,6 +11,31 @@ class TabsScreen extends StatefulWidget {
 class _TabsScreenState extends State<TabsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Meals'),
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.category),
+                text: 'Categories',
+              ),
+              Tab(
+                icon: Icon(Icons.star),
+                text: 'Favorites',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            CategoriesScreen(),
+            FavoritesScreen(),
+          ],
+        ),
+      ),
+    );
   }
 }
